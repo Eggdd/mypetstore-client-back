91f439b1a57b15ac81a26f027fb3138f2ac986c7
xianyutian <2757334535@qq.com>
2022-04-11 19:48:57 +0800

    初始化项目

---
+++ src/main/java/org/csu/mypetstoreclient/MypetstoreClientBackApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstoreclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreClientBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreClientBackApplication.class, args);
+    }
+
+}
